3416e204247d4144a58bbe6305753e17a11e94c6
dinethramudalige <dinethram88@gmail.com>
2024-08-29 17:53:35 -0600

    New repository created.

---
+++ src/main/java/com/elect/tracking/system/TrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.elect.tracking.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingSystemApplication.class, args);
+	}
+
+}
